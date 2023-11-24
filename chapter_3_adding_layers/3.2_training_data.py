180a822d6864ace16c0dacba7c776458c49b6ff0
rafsunsheikh <201614123@student.mist.ac.bd>
2023-11-24 10:40:43 +0600

    COmpleted Chapter 3

---
+++ chapter_3_adding_layers/3.2_training_data.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import nnfs
+from nnfs.datasets import spiral_data
+
+nnfs.init()
+
+X,y = spiral_data(samples = 100, classes = 3)
+
+plt.scatter(X[:,0], X[:,1], c=y, cmap = 'brg')
+plt.show()
